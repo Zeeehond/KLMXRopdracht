dd837a57828758d593666609009137d31ff144ff
Tessa Tromp <stijntromp@chello.nl>
2022-11-16 19:22:38 +0100

    Added Airplane scriptable object script and menu entry to create a scriptable object airplane in the editor

---
+++ Assets/Scripts/AirplaneScriptableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AirplaneScriptableObject", menuName = "ScriptableObjects/Airplane")]
+public class AirplaneScriptableObject : ScriptableObject
+{
+    private string airplaneType = "Fokker F.XXXVI";
+    private string airplaneCompany = "KLM";
+
+
+}
