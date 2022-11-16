e0de932385f1107ddf7adcbb5dbf9b2a27b9138a
Tessa Tromp <stijntromp@chello.nl>
2022-11-16 22:03:09 +0100

    Airplanes get numbers assigned to them that are displayed above them

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,11 +6,13 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     private int hangarNumber = 1;
+    private int airplaneNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         AssignHangarNumbers();
+        AssignAirplaneNumbers();
     }
 
     void AssignHangarNumbers()
@@ -21,6 +23,16 @@ public class GameManager : MonoBehaviour
             TextMeshPro textMeshPro = hangar.transform.Find("HangarNumber").GetComponent<TextMeshPro>();
             textMeshPro.SetText("{0}", hangarNumber);
             hangarNumber++;
+        }
+    }
+    void AssignAirplaneNumbers()
+    {
+        GameObject[] airplaneObjects = GameObject.FindGameObjectsWithTag("Airplane");
+        foreach (GameObject airplane in airplaneObjects)
+        {
+            TextMeshPro textMeshPro = airplane.transform.Find("AirplaneNumber").GetComponent<TextMeshPro>();
+            textMeshPro.SetText("{0}", airplaneNumber);
+            airplaneNumber++;
         }
     }
 
